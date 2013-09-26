05c4d3805f29e93350ea1d50fbf998d94cc6bfed
Jesse Hendrickson <jmhendrickson@gmail.com>
2013-09-26 11:29:31 -0400

    Initial commit.

---
+++ gen/me/jmhend/ui/pinchlist/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package me.jmhend.ui.pinchlist;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/me/jmhend/ui/pinchlist/PinchUtils.java
@@ -0,0 +1,22 @@
+package me.jmhend.ui.pinchlist;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Utility methods for Pinching.
+ * 
+ * @author jmhend
+ *
+ */
+public class PinchUtils {
+	
+	/**
+	 * @param dp
+	 * @param context
+	 * @return The value of 'dp' in pixels.
+	 */
+	public static int dpToPx(int dp, Context context) {
+		return (int) (dp * context.getResources().getDisplayMetrics().density + 0.5);
+	}
+}
